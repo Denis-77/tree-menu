ee4adde45b28857531f90e70d3afaeb24bf5c486
denis <academic666@mail.ru>
2023-10-29 23:14:19 +0100

    done

--- menu/app_menu/models.py
+++ menu/app_menu/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class MenuItem(models.Model):
     title = models.CharField(max_length=100)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    url = models.CharField(max_length=200, blank=True)
-    named_url = models.CharField(max_length=100, blank=True)
+    named_url = models.CharField(max_length=100)
     menu_name = models.CharField(max_length=100)
 
     def __str__(self):
